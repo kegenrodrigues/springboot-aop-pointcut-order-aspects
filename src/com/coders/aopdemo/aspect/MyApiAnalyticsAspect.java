dfcf5be6181508777140847b6b51ef64b6ce00a4
Kegen Rodrigues <kegenrodrigues95@gmail.com>
2020-06-10 15:21:20 +0530

    Order the aspects

--- src/com/coders/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/com/coders/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,13 +2,15 @@ package com.coders.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 
-	@Before("forDaoPackageNoGettersAndSetters()")
+	@Before("com.coders.aopdemo.aspect.AopExpressions.forDaoPackageNoGettersAndSetters()")
 	public void myApiAnalytics() {
 		System.out.println("Buddy we are in the logging Aspect. myApiAnalytics going on");
 	}
--- src/com/coders/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/coders/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,15 @@ package com.coders.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 
-	@Before("forDaoPackageNoGettersAndSetters()")
+	@Before("com.coders.aopdemo.aspect.AopExpressions.forDaoPackageNoGettersAndSetters()")
 	public void runBeforeAddAccount() {
 		System.out.println("Buddy we are in the logging Aspect. runBeforeAddAccount");
 	}
